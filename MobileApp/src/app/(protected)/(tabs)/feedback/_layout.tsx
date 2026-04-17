6d5c25a65cf03c4724b9ad49c4d60bd84b580078
Gevv <george.babajanyan@gmail.com>
2026-04-17 20:51:21 +0400

    feedback refresh pt.2

--- MobileApp/src/app/(protected)/(tabs)/feedback/_layout.tsx
+++ MobileApp/src/app/(protected)/(tabs)/feedback/_layout.tsx
@@ -1,14 +1,7 @@
 import { Stack } from "expo-router";
 import React from "react";
 const _layout = () => {
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="Pending" />
-      <Stack.Screen name="Completed" />
-      <Stack.Screen name="Search" />
-    </Stack>
-  );
+  return <Stack screenOptions={{ headerShown: false }} />;
 };
 
 export default _layout;
