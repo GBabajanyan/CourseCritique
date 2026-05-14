7989801296aef638cc262b6065f519165355134a
Gevv <george.babajanyan@gmail.com>
2026-05-14 22:09:30 +0400

    host DB to Neon

--- backend/db-config.js
+++ backend/db-config.js
@@ -3,11 +3,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  connectionString: process.env.DB_URL,
+  ssl: true,
+  max: 5,
+  idleTimeoutMillis: 30000,
 });
 
 export default pool;
