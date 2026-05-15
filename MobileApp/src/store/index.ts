9246a8c961254a305f1b4f979146fa6a648654b1
Gevv <george.babajanyan@gmail.com>
2026-05-15 21:32:30 +0400

    android build

--- MobileApp/src/store/index.ts
+++ MobileApp/src/store/index.ts
@@ -17,7 +17,7 @@ export class RootStore {
   constructor() {
     makeAutoObservable(this, {}, { autoBind: true });
     this.apiClient = new ApiClient(
-      process.env.EXPO_PUBLIC_API_URL || "http://localhost:8000",
+      process.env.API_URL || "http://localhost:8000",
     );
 
     this.notificationsStore = new NotificationsStore(this);
